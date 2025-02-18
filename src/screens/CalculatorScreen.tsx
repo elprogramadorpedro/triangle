75d05bc4cc70098a8f871b5bc4f6479282da572a
pedro <elprogramadorpedro@gmail.com>
2025-02-18 10:58:28 -0300

    Calculator base number construction

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { colors, styles } from '../config/theme/app-theme'
 import { CalculatorBotton }   from '../components/CalculatorBotton'
+import { useCalculator } from '../hook/useCalculator'
 
 export const CalculatorScreen = () => {
+
+  const {
+    //properties
+   number, buildNumber
+
+
+
+//methods
+  } = useCalculator();
+
+
   return (
     <View style={styles.calculatorContainer}>
        
 <View style={{paddingHorizontal:30, paddingBottom:20}}>
-        <Text style={styles.mainResult}>1500</Text>
+
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={styles.mainResult}>{number}</Text>
         <Text style={styles.subResult}>15</Text>
 </View>
     
@@ -21,15 +37,13 @@ export const CalculatorScreen = () => {
 <CalculatorBotton onPress={()=> console.log('+/-')}  blackText label="+/-" color={colors.lightGray}/>
 <CalculatorBotton onPress={()=> console.log('%')}  blackText label="%" color={colors.lightGray}/>
 <CalculatorBotton onPress={()=> console.log('÷')}  label="÷" color={colors.orange}/>
-
-
 </View>
 <View style={styles.row}>
 
 
-<CalculatorBotton onPress={()=> console.log('7')}  label="7" />
-<CalculatorBotton onPress={()=> console.log('8')}  label="8" />
-<CalculatorBotton onPress={()=> console.log('9')}  label="9" />
+<CalculatorBotton onPress={()=> buildNumber('7')}  label="7" />
+<CalculatorBotton onPress={()=> buildNumber('8')}  label="8" />
+<CalculatorBotton onPress={()=> buildNumber('9')}  label="9" />
 <CalculatorBotton onPress={()=> console.log('X')}  label="X" color={colors.orange}/>
 
 
@@ -37,9 +51,9 @@ export const CalculatorScreen = () => {
 <View style={styles.row}>
 
 
-<CalculatorBotton onPress={()=> console.log('4')}  label="4" />
-<CalculatorBotton onPress={()=> console.log('5')}  label="5"/>
-<CalculatorBotton onPress={()=> console.log('6')}  label="6" />
+<CalculatorBotton onPress={()=> buildNumber('4')}  label="4" />
+<CalculatorBotton onPress={()=> buildNumber('5')}  label="5"/>
+<CalculatorBotton onPress={()=> buildNumber('6')}  label="6" />
 <CalculatorBotton onPress={()=> console.log('-')}  label="-" color={colors.orange} />
 
 
@@ -47,9 +61,9 @@ export const CalculatorScreen = () => {
 <View style={styles.row}>
 
 
-<CalculatorBotton onPress={()=> console.log('1')}  label="1"/>
-<CalculatorBotton onPress={()=> console.log('2')}  label="2" />
-<CalculatorBotton onPress={()=> console.log('3')}  label="3" />
+<CalculatorBotton onPress={()=> buildNumber('1')}  label="1"/>
+<CalculatorBotton onPress={()=> buildNumber('2')}  label="2" />
+<CalculatorBotton onPress={()=> buildNumber('3')}  label="3" />
 <CalculatorBotton onPress={()=> console.log('+')}  label="+" color={colors.orange}/>
 
 
@@ -59,8 +73,8 @@ export const CalculatorScreen = () => {
 
 
 
-<CalculatorBotton onPress={()=> console.log('0')}  label="0" doubleSize/>
-<CalculatorBotton onPress={()=> console.log('.')}  label="." />
+<CalculatorBotton onPress={()=> buildNumber('0')}  label="0" doubleSize/>
+<CalculatorBotton onPress={()=> buildNumber('.')}  label="." />
 <CalculatorBotton onPress={()=> console.log('=')}  label="=" color={colors.orange}/>
 
 
